0ac905fa02e9d04288798ba69946c5ebdd6bc100
Vijay Stroup <vijaystroup@gmail.com>
2021-10-28 13:38:16 -0400

    readonly command

---
+++ commands/readonly.js
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+
+const ReadOnly = {
+  builder: new SlashCommandBuilder()
+    .setName('readonly')
+    .setDescription('Set a user to readonly mode.')
+    .addUserOption(option => 
+      option.setName('user')
+        .setDescription('User to assign readonly to.')
+        .setRequired(true)
+    )
+    .setDefaultPermission(false),
+  roles: ['Admin'],
+  async execute(interaction) {
+    const target = interaction.options.getMember('user')
+    const roles = interaction.guild.roles.cache
+
+    await target.roles.add(roles.find(role => role.name === 'readonly'))
+
+    await interaction.reply({ content: `${target.user.username} is now in readonly mode.` })
+  }
+}
+
+export default ReadOnly
