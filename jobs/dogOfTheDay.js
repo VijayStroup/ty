a824cfd18e1b3accd91fe3855c81c0e6393ae7c1
Vijay Stroup <vijaystroup@gmail.com>
2021-11-04 17:41:10 -0400

    dog of the day

---
+++ jobs/dogOfTheDay.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const url = 'https://dog.ceo/api/breeds/image/random'
+
+const DogOfTheDay = {
+  cronPattern: '0 9 * * *', // every day at 09:00
+  channel: '🐕｜pets',
+  async execute(channel) {
+    const res = await axios.get(url)
+  
+    if (res.status !== 200) {
+      console.error(`[${new Date().toString()}] Error getting dog of the day`)
+      return
+    }
+
+    await channel.send({files: [res.data.message]})
+  }
+}
+
+export default DogOfTheDay
