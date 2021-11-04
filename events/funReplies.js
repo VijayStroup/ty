0adcbf3e71047743f9693eec7923e066a93d603e
Vijay Stroup <vijaystroup@gmail.com>
2021-11-04 15:40:36 -0400

    fun replies event

---
+++ events/funReplies.js
@@ -0,0 +1,19 @@
+const FunReplies = {
+  name: 'messageCreate',
+  async execute(message) {
+    if (message.author.bot) return
+
+    const loweredMessage = message.content.toLowerCase()
+
+    if (loweredMessage.split(' ')[0] === 'who')
+      await message.reply('https://tenor.com/view/nba-shaquille-o-neal-yo-moma-point-laugh-gif-4759702')
+    else if (loweredMessage === 'yo')
+      await message.reply('yo')
+    else if (loweredMessage === 'based')
+      await message.reply('based on what?')
+    else if (`${loweredMessage.split(' ')[0]} ${loweredMessage.split(' ')[1]}` === 'homie said')
+      await message.reply(`homie said "${message.content}"`) 
+  }
+}
+
+export default FunReplies
