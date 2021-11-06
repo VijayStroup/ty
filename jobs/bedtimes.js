4d8b132b142dfe3e66548b7199276154c502e639
Vijay Stroup <vijaystroup@gmail.com>
2021-11-05 20:53:41 -0400

    bedtime job

---
+++ jobs/bedtimes.js
@@ -0,0 +1,22 @@
+import moment from 'moment'
+
+const bedtimes = {
+  0: [177940582226722816],
+  9: [81187850996953088],
+  21: [841043611414954034]
+}
+
+const Bedtimes = {
+  cronPattern: '* * * * *', // every minute
+  channel: '💬｜general',
+  async execute(channel) {
+    const hour = moment().tz('America/New_York').hour()
+
+    if (bedtimes[hour]) {
+      for (const id of bedtimes[hour])
+        await channel.send(`go to cleep <@${id}>`)
+    }
+  }
+}
+
+export default Bedtimes
