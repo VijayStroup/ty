804ad0f959398c9cd04450866f0d61f9bff0913f
Vijay Stroup <vijaystroup@gmail.com>
2021-11-06 21:43:08 -0400

    coin and message content cut

--- events/funReplies.js
+++ events/funReplies.js
@@ -3,16 +3,19 @@ const FunReplies = {
   async execute(message) {
     if (message.author.bot) return
 
+    const coin = (Math.floor(Math.random() * 2) == 0)
     const loweredMessage = message.content.toLowerCase()
 
-    if (loweredMessage.split(' ')[0] === 'who')
-      await message.reply('https://tenor.com/view/nba-shaquille-o-neal-yo-moma-point-laugh-gif-4759702')
-    else if (loweredMessage === 'yo')
-      await message.reply('yo')
-    else if (loweredMessage === 'based')
-      await message.reply('based on what?')
-    else if (`${loweredMessage.split(' ')[0]} ${loweredMessage.split(' ')[1]}` === 'homie said')
-      await message.reply({ content: `homie said "${message.content}"`, allowedMentions: {parse: []} }) 
+    if (coin) {
+      if (loweredMessage.split(' ')[0] === 'who')
+        await message.reply('https://tenor.com/view/nba-shaquille-o-neal-yo-moma-point-laugh-gif-4759702')
+      else if (loweredMessage === 'yo')
+        await message.reply('yo')
+      else if (loweredMessage === 'based')
+        await message.reply('based on what?')
+      else if (`${loweredMessage.split(' ')[0]} ${loweredMessage.split(' ')[1]}` === 'homie said')
+        await message.reply({ content: `homie said "${message.content.length <= 2000 ? message.content : 'that message too long homie'}"`, allowedMentions: {parse: []} }) 
+    }
   }
 }
 
