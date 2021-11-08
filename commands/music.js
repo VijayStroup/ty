987d04893bfe46135ed8bd57f9f83cfda857739b
Vijay Stroup <vijaystroup@gmail.com>
2021-11-07 20:43:15 -0500

    more music commands

--- commands/music.js
+++ commands/music.js
@@ -12,6 +12,21 @@ const Music = {
     )
     .addSubcommand(subcommand =>
       subcommand.setName('stop').setDescription('Stop playing.')
+    )
+    .addSubcommand(subcommand =>
+      subcommand.setName('skip').setDescription('Skip current song.')
+    )
+    .addSubcommand(subcommand =>
+      subcommand.setName('pause').setDescription('Pause the current song.')
+    )
+    .addSubcommand(subcommand =>
+      subcommand.setName('resume').setDescription('Resume the current song.')
+    )
+    .addSubcommand(subcommand =>
+      subcommand.setName('queue').setDescription('Get the current queue.')
+    )
+    .addSubcommand(subcommand =>
+      subcommand.setName('clearqueue').setDescription('Clear the queue.')
     ),
   channels: ['🎶｜music'],
   async execute(interaction) {
@@ -49,6 +64,67 @@ const Music = {
       }
 
       break
+    case 'skip':
+      try {
+        await music.skip({ interaction: interaction })
+        await interaction.editReply('Skipping the current song.')
+      } catch (error) {
+        await interaction.editReply({content: 'Nothing to skip.', ephemeral: true })
+      }
+
+      break
+    case 'pause':
+      try {
+        await music.pause({ interaction: interaction })
+        await interaction.editReply('Pausing the current song.')
+      } catch (error) {
+        await interaction.editReply({content: 'Nothing to pause.', ephemeral: true })
+      }
+
+      break
+    case 'resume':
+      try {
+        await music.resume({ interaction: interaction })
+        await interaction.editReply('Resuming current song.')
+      } catch (error) {
+        await interaction.editReply({content: 'Nothing to resume.', ephemeral: true })
+      }
+
+      break
+    case 'queue': {
+      try {
+        const queue = await music.getQueue({ interaction: interaction })
+        let msg = ''
+
+        queue.forEach((q, i) => {
+          msg += `${i+1}: ${q.info.title}\n`
+        })
+
+        interaction.editReply(`\`\`\`${msg}\`\`\``)
+      } catch (error) {
+        await interaction.editReply('The queue is empty.')
+      }
+
+      break
+    }
+    case 'clearqueue': {
+      try {
+        const queue = await music.getQueue({ interaction: interaction })
+  
+        for (let i; i < queue.length; i++) {
+          await music.removeQueue({
+            interaction: interaction,
+            number: i+1 
+          })
+        }
+  
+        await interaction.editReply('The queue has been cleared.')
+      } catch (error) {
+        await interaction.editReply('The queue is empty.')
+      }
+
+      break
+    }
     default:
       await interaction.editReply('Invalid subcommand.')
     }
