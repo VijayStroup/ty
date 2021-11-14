600f641b5256dd00c544425cefb9efc3a0443a7f
Vijay Stroup <vijaystroup@gmail.com>
2021-11-13 19:47:52 -0500

    ignore message deleted from bot

--- events/iSawThat.js
+++ events/iSawThat.js
@@ -1,6 +1,7 @@
 const ISawThat = {
   name: 'messageDelete',
   async execute(message) {
+    if (message.author.bot) return
     await message.channel.send(`i saw that <@${message.author.id}>`)
   }
 }
