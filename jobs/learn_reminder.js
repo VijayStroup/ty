8cb017ed80861853c045a3b16f5ff5ad3a5ce0af
Warren Snipes <contact@warrensnipes.dev>
2021-12-02 15:35:22 -0500

    Fixed chnages

--- jobs/learn_reminder.js
+++ jobs/learn_reminder.js
@@ -1,12 +1,11 @@
-const users = ['864311867765686332', '653079222088106005']
 // Kiwi & Lemon
+const users = ['864311867765686332', '653079222088106005']
 const LearnReminder = {
   cronPattern: '0 * * * *', // every hour at minute 0
   channel: '💬｜general',
   async execute(channel) {
-    for (let user of users) {
+    for (let user of users)
       await channel.send(`Go learn how to code <@${user}>`)
-    }
   }
 }
 
