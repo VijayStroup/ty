e57aa1aabd3956d2eb6e0109b5652e9bbe4781ae
Vijay Stroup <vijaystroup@gmail.com>
2022-04-10 14:53:04 -0400

    ignored isawthat

--- events/iSawThat.js
+++ events/iSawThat.js
@@ -1,7 +1,10 @@
+const ignored = ['864311867765686332', '369212248553422850']
+
 const ISawThat = {
   name: 'messageDelete',
   async execute(message) {
     if (message.author.bot) return
+    if (ignored.includes(message.author.id)) return
     await message.channel.send(`i saw that <@${message.author.id}>`)
   }
 }
