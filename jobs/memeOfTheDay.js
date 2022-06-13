862009ad8cd5388ac5063c8c47a1775d84e224dd
Vijay Stroup <vijaystroup@gmail.com>
2022-06-13 18:26:02 -0400

    fix failed to upload errors

--- jobs/memeOfTheDay.js
+++ jobs/memeOfTheDay.js
@@ -2,19 +2,33 @@ import axios from 'axios'
 
 const url = 'https://meme-api.herokuapp.com/gimme'
 
-const DogOfTheDay = {
+async function getMeme() {
+  const res = await axios.get(url)
+
+  if (res.status !== 200) {
+    console.error(`[${new Date().toString()}] Error getting meme of the day`)
+    return null
+  }
+
+  return res.data.url
+}
+
+const MemeOfTheDay = {
   cronPattern: '0 18 * * *', // every day at 18:00
   channel: '🤣｜memes',
   async execute(channel) {
-    const res = await axios.get(url)
-
-    if (res.status !== 200) {
-      console.error(`[${new Date().toString()}] Error getting meme of the day`)
-      return
+    let success = false
+    while (!success) {
+      try {
+        const meme = await getMeme()
+        if (meme === null) throw new Error('Didn\'t get a meme.')
+        else {
+          await channel.send({files: [meme]})
+          success = true
+        }
+      } catch(e) { /* get another one */ }
     }
-
-    await channel.send({files: [res.data.url]})
   }
 }
 
-export default DogOfTheDay
+export default MemeOfTheDay
