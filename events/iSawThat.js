20174db0ff949c9a2b1378ae3bb87a451f80693d
Jason "Jay" Davis <68671835+MrJay-exe@users.noreply.github.com>
2022-07-11 14:37:50 -0400

    added random to i saw that.

--- events/iSawThat.js
+++ events/iSawThat.js
@@ -1,11 +1,16 @@
-const ignored = ['864311867765686332', '369212248553422850']
+const ignored = ['864311867765686332', '369212248553422850','609330932431716353']
 
 const ISawThat = {
   name: 'messageDelete',
   async execute(message) {
     if (message.author.bot) return
+
+    const coin = (Math.floor(Math.random() * 2) == 0)
+
+    if(coin){
     if (ignored.includes(message.author.id)) return
     await message.channel.send(`i saw that <@${message.author.id}>`)
+    }
   }
 }
 
